46a44fef61ed89ce6e5b9c352e158fed5cdb97c0
Mateusz <lodowy123@o2.pl>
2017-03-01 21:41:34 +0100

    sqlScript

--- src/main/resources/prepareDatabase.sql
+++ src/main/resources/prepareDatabase.sql
@@ -1,11 +1,40 @@
-CREATE TABLE user (idUser int(10),
-                    username VARCHAR(30),
-                    active int(1),
-                    role VARCHAR(10),
-                    password varchar(20),
-                    email VARCHAR(30)
+CREATE TABLE message
+(
+    idMessage INT(11) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    text VARCHAR(2048) NOT NULL,
+    idUser INT(11) NOT NULL,
+    authorName VARCHAR(30) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
-
-CREATE TABLE topic (idTopic int(10),
-                    title varchar(100),
-                    )
+CREATE TABLE post
+(
+    idPost INT(11) NOT NULL,
+    text VARCHAR(2048) NOT NULL,
+    number INT(2) NOT NULL,
+    idTopic INT(11) NOT NULL,
+    idUser INT(11) NOT NULL,
+    date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+CREATE TABLE section
+(
+    idSection INT(11) NOT NULL,
+    name VARCHAR(201),
+    idParent INT(11)
+);
+CREATE TABLE topic
+(
+    idTopic INT(11) NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    idUser INT(11) NOT NULL,
+    idSection INT(11) NOT NULL
+);
+CREATE TABLE user
+(
+    idUser INT(10) NOT NULL,
+    active TINYINT(1) NOT NULL,
+    role VARCHAR(10) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(30) NOT NULL
+);
